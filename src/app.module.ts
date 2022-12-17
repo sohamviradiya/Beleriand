e7978e60436ba26a36bdce7a8d8cb5eb6e76a30f
sohamviradiya <sohamviradiya2003@gmail.com>
2022-12-17 15:20:06 +0530

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,11 @@ import { CommentsController } from './comments/comments.controller';
 import { UsersService } from './users/users.service';
 import { TweetsService } from './tweets/tweets.service';
 import { CommentsService } from './comments/comments.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [],
-	controllers: [
-		AppController,
-		UsersController,
-		TweetsController,
-		CommentsController,
-	],
+	imports: [MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })],
+	controllers: [AppController, UsersController, TweetsController, CommentsController],
 	providers: [AppService, UsersService, TweetsService, CommentsService],
 })
 export class AppModule {}
